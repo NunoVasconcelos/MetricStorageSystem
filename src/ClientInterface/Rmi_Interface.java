71da5b6f01c57c6e7fa86c6355b3cd019ced3789
Nuno Rogério Fernandes Vasconcelos <nunorogerio12@hotmail.com>
2016-05-11 23:00:55 +0100

    RMI Interface changed

--- src/ClientInterface/Rmi_Interface.java
+++ src/ClientInterface/Rmi_Interface.java
@@ -1,9 +1,14 @@
 package ClientInterface;
 
+import Server.Metrics;
+import com.sun.org.apache.xpath.internal.operations.Bool;
+
+import java.math.BigInteger;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface Rmi_Interface extends Remote {
 
-     String addToList(int a)  throws RemoteException;
+    Boolean contains(BigInteger n) throws RemoteException;
+    Metrics getMetrics(BigInteger n) throws RemoteException;
 }
--- src/Server/StorageSystem.java
+++ src/Server/StorageSystem.java
@@ -1,17 +1,21 @@
 package Server;
 import ClientInterface.Rmi_Interface;
+
+import java.math.BigInteger;
 import java.rmi.Naming;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 
 public class StorageSystem extends UnicastRemoteObject implements Rmi_Interface {
     public StorageSystem() throws RemoteException {
     }
 
-    ArrayList<Integer> array = new ArrayList<>();
+    HashMap<BigInteger, Metrics> hashMap = new HashMap<>();
+
     public static void main (String argv[]) throws RemoteException
     {
         try
@@ -27,12 +31,18 @@ public class StorageSystem extends UnicastRemoteObject implements Rmi_Interface 
         System.out.println("RmiServer started..");
         System.out.println("Listening..");
     }
-    @Override
-    public synchronized String addToList(int a) {
-        array.add(a);
-        System.out.println(array.toString());
-        return "success!";
+
+
+    public Boolean contains(BigInteger n)
+    {
+        return hashMap.containsKey(n);
     }
+
+    public Metrics getMetrics(BigInteger n)
+    {
+        return hashMap.get(n);
+    }
+
 
 
 }
